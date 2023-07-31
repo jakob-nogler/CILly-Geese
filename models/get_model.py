178f1412197f22abbc2a68575e27269edc4348cb
rayz1065 <rayz1065@gmail.com>
2023-07-31 12:55:34 +0200

    Updated some default configs, updated get_model function

--- models/get_model.py
+++ models/get_model.py
@@ -18,24 +18,19 @@ if typing.TYPE_CHECKING:
 def get_model(name: str, hyperparameters: dict, train_set: "pd.DataFrame", test_set: "pd.DataFrame") -> Model:
     """Returns a model based on the name string -> initialized with the specified parameters
     """
-    if name == "autoencoder":
-        return Autoencoder(hyperparameters, train_set, test_set)
-    elif name == "ncf":
-        return NCF(hyperparameters, train_set, test_set)
-    elif name == "als":
-        return ALSModel(hyperparameters, train_set, test_set)
-    elif name == "vae":
-        return VAE(hyperparameters, train_set, test_set)
-    elif name == "lightgcn":
-        return LightGCN(hyperparameters, train_set, test_set)
-    elif name == "funkSVD":
-        return funkSVD(hyperparameters, train_set, test_set)
-    elif name == "SVDplusplus":
-        return SVDplusplus(hyperparameters, train_set, test_set)
-    elif name == "oneidea" or name == "cfda":
-        return CFDA(hyperparameters, train_set, test_set)
-    elif name == "gernot":
-        return Gernot(hyperparameters, train_set, test_set)
-    elif name == "bayesiansvd":
-        return BayesianSVD(hyperparameters, train_set, test_set)
-    return None
+    MODEL_CLASSES = {
+        'autoencoder': Autoencoder,
+        'ncf': NCF,
+        'als': ALSModel,
+        'vae': VAE,
+        'lightgcn': LightGCN,
+        'funksvd': funkSVD,
+        'svdplusplus': SVDplusplus,
+        'oneidea': CFDA,
+        'cfda': CFDA,
+        'gernot': Gernot,
+        'bayesiansvd': BayesianSVD,
+    }
+
+    model_class = MODEL_CLASSES[name.lower()]
+    return model_class(hyperparameters, train_set, test_set)
